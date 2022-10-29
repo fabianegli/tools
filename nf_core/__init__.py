4b27230ab5936801d326270958552cb5b4ebaa2f
fabianegli <fabian.egli@protonmail.ch>
2022-10-29 14:32:38 +0200

    hide pkg_resources from nf-core api

--- nf_core/__init__.py
+++ nf_core/__init__.py
@@ -4,6 +4,6 @@
 Shouldn't do much, as everything is under subcommands.
 """
 
-import pkg_resources
+import pkg_resources as _pkg_resources
 
-__version__ = pkg_resources.get_distribution("nf_core").version
+__version__ = _pkg_resources.get_distribution("nf_core").version
